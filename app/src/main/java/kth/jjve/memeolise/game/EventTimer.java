168b3fbc578c4183c3caa045cc32a55b725ba94c
Jitse vE <j.j.vanesch@student.utwente.nl>
2021-11-25 23:38:26 +0100

    Added a timer/runner to the game activity

---
+++ app/src/main/java/kth/jjve/memeolise/game/EventTimer.java
@@ -0,0 +1,9 @@
+package kth.jjve.memeolise.game;
+
+import android.util.Log;
+
+import java.util.TimerTask;
+
+public class EventTimer {
+//Todo: see if we can put the stuff from the gameActivity in this class
+}
